b94cfed1944e6702f29d393d595898c0bf0d00a7
Varga Attila Tibor <h053414@stud.u-szeged.hu>
2023-04-15 21:15:55 +0200

    - design components - install firebase and set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment.development';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyD5FqXsjJzob8CmL2DYpEgHeslsceOnOvM",
+    authDomain: "ang-blog-stud-project.firebaseapp.com",
+    projectId: "ang-blog-stud-project",
+    storageBucket: "ang-blog-stud-project.appspot.com",
+    messagingSenderId: "277287583347",
+    appId: "1:277287583347:web:4d16c67d906da2c5a87406"
+  }
+};
